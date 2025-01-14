3094f6c5cec5a5c96b647c773f68169ae49f19ff
AnushikaSinha <sinhaanushika@yahoo.in>
2025-01-15 05:03:50 +0530

    Add the custom logger

--- index.js
+++ index.js
@@ -1,8 +1,29 @@
-import 'dotenv/config'
+import "dotenv/config";
 import express from "express";
+import logger from "./logger.js";
+import morgan from "morgan";
+
 const app = express();
 const port = process.env.PORT || 3000;
 app.use(express.json());
+
+const morganFormat = ":method :url :status :response-time ms";
+
+app.use(
+  morgan(morganFormat, {
+    stream: {
+      write: (message) => {
+        const logObject = {
+          method: message.split(" ")[0],
+          url: message.split(" ")[1],
+          status: message.split(" ")[2],
+          responseTime: message.split(" ")[3],
+        };
+        logger.info(JSON.stringify(logObject));
+      },
+    },
+  })
+);
 
 /*app.get("/", (req, res) => {
   res.send("Hello Demo Express!");
@@ -38,6 +59,7 @@ let nextId = 1;
 
 //Add hobbies
 app.post("/hobbies", (req, res) => {
+  logger.info("A post request was made")
   const { name, level } = req.body;
   const newHobby = { id: nextId++, name, level };
   hobby.push(newHobby);
@@ -51,34 +73,34 @@ app.get("/hobbies", (req, res) => {
 
 //Get a hobby
 app.get("/hobbies/:id", (req, res) => {
-    const currentHobby = hobby.find(hob => hob.id === parseInt(req.params.id))
-    if(!currentHobby){
-  return res.status(404).send("Hobby Not Found");
-    }
-    res.status(200).send(currentHobby);
+  const currentHobby = hobby.find((hob) => hob.id === parseInt(req.params.id));
+  if (!currentHobby) {
+    return res.status(404).send("Hobby Not Found");
+  }
+  res.status(200).send(currentHobby);
 });
 
 //Update a hobby
-app.put('/hobbies/:id', (req, res) => {
-    const currentHobby = hobby.find(hob => hob.id === parseInt(req.params.id))
-    if (!currentHobby) {
-      return res.status(404).send("Hobby Not Found");
-    }
-    const {name, level} = req.body
-    currentHobby.name = name
-    currentHobby.level = level
-    res.status(200).send(currentHobby);
-})
+app.put("/hobbies/:id", (req, res) => {
+  const currentHobby = hobby.find((hob) => hob.id === parseInt(req.params.id));
+  if (!currentHobby) {
+    return res.status(404).send("Hobby Not Found");
+  }
+  const { name, level } = req.body;
+  currentHobby.name = name;
+  currentHobby.level = level;
+  res.status(200).send(currentHobby);
+});
 
 //Delete a hobby
-app.delete('/hobbies/:id', (req, res) =>{
-    hobby.findIndex(hob => hob.id === parseInt(req.params.id))
-    if(index === -1){
-        return res.status(404).send("Hobby Not Found")
-    }
-    hobby.splice(index, 1)
-    return res.status(200).send("Deleted")
-})
+app.delete("/hobbies/:id", (req, res) => {
+  hobby.findIndex((hob) => hob.id === parseInt(req.params.id));
+  if (index === -1) {
+    return res.status(404).send("Hobby Not Found");
+  }
+  hobby.splice(index, 1);
+  return res.status(200).send("Deleted");
+});
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}...`);
